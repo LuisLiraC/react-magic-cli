796151eba80f9104b51f489aeb6ed31483b21741
LuisLiraC <luis_ssbm@hotmail.com>
2022-01-02 16:24:49 -0600

    TS Component

--- commands/component/index.js
+++ commands/component/index.js
@@ -3,13 +3,18 @@ const path = require('path')
 const CURRENT_DIR = process.cwd()
 
 function create (componentName) {
-  const language = 'javascript'
-  const templatePath = path.join(__dirname, `/../../templates/${language}/component/index.template`)
+  const files = fs.readdirSync(CURRENT_DIR)
+
+  const { language, extension } = files.includes('tsconfig.json')
+    ? { language: 'typescript', extension: 'tsx' }
+    : { language: 'javascript', extension: 'jsx' }
+
+  const templatePath = path.join(__dirname, `/../../templates/${language}/component/index.${extension}`)
 
   let contents = fs.readFileSync(templatePath, 'utf-8')
   contents = contents.replace(/COMPONENT_NAME/g, componentName)
 
-  const newFileName = `${componentName}.jsx`
+  const newFileName = `${componentName}.${extension}`
   const writePath = `${CURRENT_DIR}/src/components/${newFileName}`
 
   fs.writeFileSync(writePath, contents, 'utf-8')
---
+++ templates/typescript/component/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface Props {
+  message?: string
+}
+
+function COMPONENT_NAME(props: Props) {
+  return (
+    <h1>COMPONENT_NAME works</h1>
+  )
+}
+
+export default COMPONENT_NAME
